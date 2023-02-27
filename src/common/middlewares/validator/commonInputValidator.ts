aa815cc4e8a7387a0829bf0fa693d06b64a9bb3b
jobayercse71 <rahmanjobayer71@gmail.com>
2023-02-28 00:56:52 +0600

    postman collection added

--- src/common/middlewares/validator/commonInputValidator.ts
+++ src/common/middlewares/validator/commonInputValidator.ts
@@ -5,16 +5,20 @@ class CommonInputValidation {
   public sentOtpPhoneInputValidator() {
     return [
       body("otp_phone", "Please provide user phone number").exists().notEmpty(),
-      body("otp_type", "Please provide otp type").exists().notEmpty(),
+      body(
+        "otp_type",
+        "Please enter valid OTP type as login or register or forget-password or phone-verify"
+      ).isIn(["login", "register", "phone-verify", "forget-password"]),
     ];
   }
 
   // match otp input validator
   public matchOtpInputValidator() {
     return [
-      body("otp_type", "Please enter valid OTP type as login or register").isIn(
-        ["login", "register", "phone-verify", "forget-password"]
-      ),
+      body(
+        "otp_type",
+        "Please enter valid OTP type as login or register or forget-password or phone-verify"
+      ).isIn(["login", "register", "phone-verify", "forget-password"]),
       body("user_phone", "Enter phone number").exists().notEmpty(),
       body("otp", "Provide otp").exists(),
     ];
